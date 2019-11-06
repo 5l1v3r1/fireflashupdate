6d9023a0aa39452dd7ad4eee87438d0039254a77
Andrea Dari <andreadari@protonmail.com>
2019-11-06 17:39:57 +0100

    Trailing spaces in the license removed.

--- fireflashupdate.sh
+++ fireflashupdate.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 ########
-#LICENSE                                                   
+#LICENSE
 ########
 
 # Adobe Flash Player installer/updater for Mozilla Firefox. Please visit the project's website at: https://github.com/cybernova/fireflashupdate
-# Copyright (C) 2018 Andrea Dari (andreadari@protonmail.com)                                   
-#                                                                                                       
-# This shell script is free software: you can redistribute it and/or modify                             
-# it under the terms of the GNU General Public License as published by                                   
-# the Free Software Foundation, either version 2 of the License, or                                     
-# any later version.                                                                   
-#                                                                                                       
-# This program is distributed in the hope that it will be useful,                                       
-# but WITHOUT ANY WARRANTY; without even the implied warranty of                                        
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                         
-# GNU General Public License for more details.                                                          
-#                                                                                                       
-# You should have received a copy of the GNU General Public License                                     
+# Copyright (C) 2018 Andrea Dari (andreadari@protonmail.com)
+#
+# This shell script is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
 # along with this shell script.  If not, see <http://www.gnu.org/licenses/>.
 
 
